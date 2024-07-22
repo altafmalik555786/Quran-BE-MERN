e19d350f4a7b4e4259575afafa6fbda25995838e
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2024-07-22 15:54:32 +0500

    login

--- utils/studentEmailVerification.js
+++ utils/studentEmailVerification.js
@@ -1,23 +1,19 @@
 const nodemailer = require('nodemailer');
-
 // Function to send verification email
 const sendVerificationEmail = async (email, name) => {
     const transporter = nodemailer.createTransport({
-        service: 'Gmail', // Or any other email service you are using
+        service: 'Gmail',
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
         }
     });
-
     const mailOptions = {
         from: process.env.EMAIL_USERNAME,
         to: email,
         subject: 'Verify Your Email',
         text: `Hello ${name},\n\nPlease verify your email by clicking the link: \nhttp://${process.env.FRONTEND_URL}/verify-email?email=${email}\n\nThank You!\n`
     };
-
     await transporter.sendMail(mailOptions);
 };
-
 module.exports = sendVerificationEmail;
