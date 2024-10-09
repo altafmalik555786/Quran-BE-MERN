57fd0415f3e507e2e5086c325034146043cf6747
Altaf Malik <altaf@envoylabs.net>
2024-10-09 06:52:58 +0500

    build for vercel

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+require('dotenv').config();
+const connectDB = require('../config/db');
+const studentRoutes = require('../routes/routes');
+const tutorRoutes = require('../routes/tutorRoutes');
+const path = require("path");
+
+// Serve the 'uploads' folder
+
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('uploads/tutor', express.static(path.join(__dirname, 'uploads/tutor')));
+dotenv.config();
+
+// Middleware
+app.use(express.json({ limit: '10mb' }));
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use('/api/v1', studentRoutes);
+app.use('/api/v1/tutor', tutorRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server started at port no. ${PORT}`);
+});
